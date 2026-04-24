695fab1fb81d36146877cb966bbe994ffe871bea
VitorSchmitt <vitorschmitt@hotmail.com>
2026-04-24 10:23:59 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:fcsllessVEooGkbu@db.pvmlhnqrnhciuqyzwnuj.supabase.co:5432/postgres"
-
 engine = create_engine(
     DATABASE_URL,
     connect_args={"sslmode": "require"}

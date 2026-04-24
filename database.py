598091db37284bec73459aac20c3c1d8506911ad
VitorSchmitt <vitorschmitt@hotmail.com>
2026-04-24 11:00:05 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 🔴 IMPORTANTE: usar pooler (IPv4)
-DATABASE_URL = "postgresql+psycopg2://postgres.pvmlhnqrnhciuqyzwnuj:fcsllessVEooGkbu@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = "postgresql://postgres.pvmlhnqrnhciuqyzwnuj:fcsllessVEooGkbu@aws-1-us-west-2.pooler.supabase.com:6543/postgres"
 
 # 🔐 SSL obrigatório no Supabase
 engine = create_engine(

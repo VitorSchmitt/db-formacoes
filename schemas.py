7267aafbc7268433b568ab76cea7726d41538b61
VitorSchmitt <vitorschmitt@hotmail.com>
2026-04-27 14:18:48 -0300

    Create schemas.py

---
+++ schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+class FormacaoBase(BaseModel):
+    descricao: str
+    data_termino: date
+    carga_horaria: int
+    modalidade: str
+    eixo: str
+
+class FormacaoCreate(FormacaoBase):
+    pass
+
+class FormacaoUpdate(BaseModel):
+    descricao: Optional[str]
+    data_termino: Optional[date]
+    carga_horaria: Optional[int]
+    modalidade: Optional[str]
+    eixo: Optional[str]

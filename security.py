f807c8320802fde178b9fb1117909dd452655f4a
VitorSchmitt <vitorschmitt@hotmail.com>
2026-05-05 14:28:20 -0300

    Update security.py

--- security.py
+++ security.py
@@ -1,3 +1,24 @@
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+
+SECRET_KEY = "super-secret-key"
+ALGORITHM = "HS256"
+EXP_MINUTES = 60
+
+def criar_token(data: dict):
+    payload = data.copy()
+    payload["exp"] = datetime.utcnow() + timedelta(minutes=EXP_MINUTES)
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+def validar_token(token: str):
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        return None

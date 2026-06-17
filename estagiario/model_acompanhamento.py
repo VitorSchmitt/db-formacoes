cf5ca6e3f52ec8f84e826f9788840fa4adde517f
VitorSchmitt <vitorschmitt@hotmail.com>
2026-06-17 11:06:58 -0300

    Create model_acompanhamento.py

---
+++ estagiario/model_acompanhamento.py
@@ -0,0 +1,36 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Date,
+    ForeignKey,
+    Text
+)
+
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class AvaliacaoSupervisor(Base):
+    __tablename__ = "avaliacoes_supervisor"
+
+    id = Column(Integer, primary_key=True)
+
+    contrato_id = Column(
+        Integer,
+        ForeignKey("contratos_estagio.id"),
+        nullable=False
+    )
+
+    data_avaliacao = Column(
+        Date,
+        nullable=False
+    )
+
+    nota = Column(Integer)
+
+    parecer = Column(Text)
+
+    contrato = relationship(
+        "ContratoEstagio"
+    )

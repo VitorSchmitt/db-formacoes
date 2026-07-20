4870439d54e1e0db87ad2d29464056dbefc77fc7
VitorSchmitt <vitorschmitt@hotmail.com>
2026-07-20 10:56:58 -0300

    Create calcular_pagamento.py

---
+++ estagiario/routes/calcular_pagamento.py
@@ -0,0 +1,75 @@
+from decimal import Decimal
+
+
+def calcular_pagamento(
+    frequencia,
+    contrato,
+    valor_hora,
+    valor_va_fixo,
+    valor_vt,
+    dias_referencia,
+    percentual_encargo
+):
+
+    valor_bolsa = (
+        Decimal(str(frequencia.horas_realizadas))
+        *
+        valor_hora
+    )
+
+
+    if contrato.vale_alimentacao:
+
+        valor_alimentacao = (
+            valor_va_fixo
+            /
+            Decimal(dias_referencia)
+        ) * Decimal(frequencia.dias)
+
+    else:
+
+        valor_alimentacao = Decimal("0.00")
+
+
+    valor_transporte = (
+        Decimal(frequencia.dias)
+        *
+        Decimal(contrato.quantidade_vale_transporte)
+        *
+        valor_vt
+    )
+
+
+    valor_total = (
+        valor_bolsa
+        +
+        valor_alimentacao
+        +
+        valor_transporte
+    )
+
+
+    valor_encargo = (
+        valor_total
+        *
+        percentual_encargo
+    )
+
+
+    return {
+
+        "valor_hora_aplicado":
+            valor_hora,
+
+        "valor_vale_alimentacao":
+            valor_alimentacao,
+
+        "valor_vale_transporte":
+            valor_transporte,
+
+        "valor_total":
+            valor_total,
+
+        "valor_encargo":
+            valor_encargo
+    }
